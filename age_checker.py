f3fdd55acf524c71cba6561a338d3438e39646b6
gourspatar <goursundarpatar2@gmail.com>
2026-07-26 20:40:35 +0530

    Delete age_checker.py

--- age_checker.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Enter Your Age :"))
-if age < 13 :
-    print("You are a child.")
-elif age >= 13 and age < 20 :
-    print("You are a teenager.")
-elif age >= 20 and age < 60 :
-    print("You are an adult.")
-else :
-    print("You are a senior citizen.")
