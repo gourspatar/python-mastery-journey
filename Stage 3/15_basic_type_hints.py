2a28f62d84d0c3a904b0c6b731dc0e10e787eefd
Gour S Patar <goursundarpatar2@gmail.com>
2026-07-30 13:41:43 +0530

    Practiced basic typr hints and typing modules

---
+++ Stage 3/15_basic_type_hints.py
@@ -0,0 +1,10 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+
+print(add(10, 5))
+print(greet("Gour"))
---
+++ Stage 3/16_typing_module.py
@@ -0,0 +1,34 @@
+from typing import Optional, List, Dict, Union
+
+
+# Optional
+def get_name(name: Optional[str]) -> str:
+    if name is None:
+        return "No name provided"
+    return name
+
+
+# List
+def sum_numbers(numbers: List[int]) -> int:
+    return sum(numbers)
+
+
+# Dict
+def get_student() -> Dict[str, int]:
+    return {"age": 20, "marks": 95}
+
+
+# Union
+def square(number: Union[int, float]) -> Union[int, float]:
+    return number ** 2
+
+
+print(get_name("Gour"))
+print(get_name(None))
+
+print(sum_numbers([1, 2, 3, 4, 5]))
+
+print(get_student())
+
+print(square(5))
+print(square(2.5))
